a6c829a1f125222e7235c1f60c289c8fafd93a09
Akshat Karnwal <akforsn@gmail.com>
2016-03-07 21:18:37 +0530

    Added the project folder.

---
+++ Project/Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.FolderPath = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkBlue
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button1.Location = New System.Drawing.Point(309, 165)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'FolderPath
+        '
+        Me.FolderPath.ForeColor = System.Drawing.Color.DarkBlue
+        Me.FolderPath.Location = New System.Drawing.Point(33, 167)
+        Me.FolderPath.Name = "FolderPath"
+        Me.FolderPath.Size = New System.Drawing.Size(270, 20)
+        Me.FolderPath.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 151)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Select a folder"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DarkBlue
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button2.Location = New System.Drawing.Point(33, 193)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(351, 23)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Arrange Files"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DarkBlue
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button3.Location = New System.Drawing.Point(390, 164)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(54, 52)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "Open"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label2.Font = New System.Drawing.Font("Supernatural Knight", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Label2.Location = New System.Drawing.Point(68, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(353, 44)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Folder Master"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClientSize = New System.Drawing.Size(470, 228)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.FolderPath)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Folder Master"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents FolderPath As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Project/Form1.vb
@@ -0,0 +1,126 @@
+﻿Imports System.IO
+Imports System.Security.AccessControl
+Public Class Form1
+
+   
+    Dim ExtArray(500) As String
+    Dim Audio As String() = {"Audio", ".mp3", ".wav", ".wma", ".rm", ".ogg", ".mid", ".m4a", ".mpa", ".m3u"}
+    Dim Video As String() = {"Video", ".wmv", ".m4v", ".vob ", ".mp4", ".mov", "mkv", ".avi", ".divx", ".mpeg", ".mpg", ".m4p", ".flv", ".webm", ".3gp"}
+    Dim Document As String() = {"Document", ".docx", ".doc", ".txt", ".xls", ".pdf", ".rtf", ".tex", ".wpd", ".xlsx", ".pptx", ".ppt"}
+    Dim Code As String() = {"Code", "vbs", ".c", ".h", ".vb", ".xhtml", ".rss", ".cpp", ".java", ".php", ".html", ".css", ".py", ".aspx", ".json", ".js", ".jsp", ".do", ".htm", ".cfm", ".xml", ".xaml", ".pl", ".asp", ".fs", ".cs", ".sln"}
+    Dim Images As String() = {"Images", ".jpg", ".jpeg", ".png", ".tiff", ".gif", ".bmp", ".svg", ".psd", ".tiff"}
+    Dim Data As String() = {"Data", ".csv", ".dat", ".raw", ".sdf", ".vcf", ".xml"}
+    Dim Database As String() = {"Database", ".db", ".accdb", ".accdc", ".adb", ".cdb", ".dbc", ".dbf", ".dbs", ".mdb", ".mdf", ".odb", ".sdb", ".sqlite", ".sqlite3", ".sqlitedb", ".xld", ".pdb"}
+    Dim Executable As String() = {"Executable", ".apk", ".exe", ".app", ".bat", ".cgi", ".com", ".gadget", ".jar", ".pif", ".wsf", ".msi", ".bin", ".inf"}
+    Dim Subtitle As String() = {"Subtitle", ".srt", "sub", ".sbv", ".ifo", ".idx"}
+    Dim Certificate As String() = {"Certificate", ".cer", ".cert", ".pem", ".ssl", ".crt", ".csr", ".pfx", ".crl", ".stl", ".key"}
+    Dim Shortcuts As String() = {"Shortcuts", ".lnk"}
+    Dim Plugins As String() = {"Plugins", ".crx", ".plugin", ".xpi"}
+    Dim Fonts As String() = {"Fonts", ".ttf", ".fnt", ".fon", ".otf"}
+    Dim SystemFile As String() = {"System", ".cab", ".sys", ".cpl", ".ico", ".drv", ".cur"}
+    Dim Config As String() = {"Config", ".cfg", ".ini", ".dll"}
+    Dim Compressed As String() = {"Compressed", ".7z", ".rar", ".zip", ".deb", ".pkg", ".tar.gz"}
+    Dim ISO As String() = {"ISO", ".iso", ".dmg", ".vcd", ".mdf"}
+    Dim Backup As String() = {"Backup", ".bak", ".tmp"}
+    Dim Torrents As String() = {"Torrents", ".torrent"}
+    Dim Misc As String() = {"Misc", ".crdownload", ".part", ".aup", ".orig", ""}
+    Dim i As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog()
+        FolderPath.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Directory.Exists(FolderPath.Text) = False Then
+            MessageBox.Show("Please enter a valid path.")
+            Exit Sub
+        End If
+        Process.Start(FolderPath.Text)
+    End Sub
+
+    Function CheckExt(ByVal DataCollection As String(), ByVal PresentCollection As String(), ByVal Path As String) As Boolean
+        Dim securityRules As DirectorySecurity = New DirectorySecurity()
+        securityRules.AddAccessRule(New FileSystemAccessRule(System.Security.Principal.WindowsIdentity.GetCurrent().Name, FileSystemRights.FullControl, AccessControlType.Allow))
+
+        For Each item As String In PresentCollection
+            If DataCollection.Contains(item) Then
+                Directory.CreateDirectory(Path & "\" & DataCollection(0), securityRules)
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Sub PlaceFiles(ByVal DataCollection As String(), ByVal file As String)
+
+        If DataCollection.Contains(System.IO.Path.GetExtension(file)) Then
+            If System.IO.File.Exists(FolderPath.Text & "\" & DataCollection(0) & "\" & System.IO.Path.GetFileName(file)) Then
+                System.IO.File.Delete(file)
+            Else
+                System.IO.File.Move(file, FolderPath.Text & "\" & DataCollection(0) & "\" & System.IO.Path.GetFileName(file))
+            End If
+
+        End If
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If Directory.Exists(FolderPath.Text) = False Then
+            MessageBox.Show("Please enter a valid path.")
+            Exit Sub
+        End If
+        Dim PathName As String = FolderPath.Text
+        Dim FileNames As String() = Directory.GetFiles(FolderPath.Text)
+        For Each FileName As String In FileNames
+            If ExtArray.Contains(Path.GetExtension(FileName)) = False Then
+                ExtArray(i) = Path.GetExtension(FileName)
+            End If
+            i = i + 1
+        Next
+
+        CheckExt(Audio, ExtArray, PathName)
+        CheckExt(Video, ExtArray, PathName)
+        CheckExt(Document, ExtArray, PathName)
+        CheckExt(Code, ExtArray, PathName)
+        CheckExt(Images, ExtArray, PathName)
+        CheckExt(Data, ExtArray, PathName)
+        CheckExt(Database, ExtArray, PathName)
+        CheckExt(Executable, ExtArray, PathName)
+        CheckExt(Compressed, ExtArray, PathName)
+        CheckExt(SystemFile, ExtArray, PathName)
+        CheckExt(Misc, ExtArray, PathName)
+        CheckExt(ISO, ExtArray, PathName)
+        CheckExt(Torrents, ExtArray, PathName)
+        CheckExt(Backup, ExtArray, PathName)
+        CheckExt(Config, ExtArray, PathName)
+        CheckExt(Subtitle, ExtArray, PathName)
+        CheckExt(Certificate, ExtArray, PathName)
+        CheckExt(Shortcuts, ExtArray, PathName)
+        CheckExt(Plugins, ExtArray, PathName)
+        CheckExt(Fonts, ExtArray, PathName)
+
+        For Each FileName As String In FileNames
+            PlaceFiles(Audio, FileName)
+            PlaceFiles(Video, FileName)
+            PlaceFiles(Document, FileName)
+            PlaceFiles(Code, FileName)
+            PlaceFiles(Images, FileName)
+            PlaceFiles(Data, FileName)
+            PlaceFiles(Database, FileName)
+            PlaceFiles(Executable, FileName)
+            PlaceFiles(Compressed, FileName)
+            PlaceFiles(SystemFile, FileName)
+            PlaceFiles(Misc, FileName)
+            PlaceFiles(ISO, FileName)
+            PlaceFiles(Torrents, FileName)
+            PlaceFiles(Backup, FileName)
+            PlaceFiles(Config, FileName)
+            PlaceFiles(Subtitle, FileName)
+            PlaceFiles(Certificate, FileName)
+            PlaceFiles(Shortcuts, FileName)
+            PlaceFiles(Plugins, FileName)
+            PlaceFiles(Fonts, FileName)
+        Next
+
+        MessageBox.Show("Your folder has been sorted!")
+    End Sub
+End Class
